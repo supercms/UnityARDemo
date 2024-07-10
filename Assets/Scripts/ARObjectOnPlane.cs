c99e96547b2b777e940a386fb55e4bd6d2e8cc41
supercms <supercms@naver.com>
2024-07-10 15:47:26 +0900

    오브젝트 놓기 진행중

---
+++ Assets/Scripts/ARObjectOnPlane.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARObjectOnPlane : MonoBehaviour
+{
+    private ARRaycastManager raycastManager;
+
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    [SerializeField]
+    Text txtStatus;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+        txtStatus.text = "Awake";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 0)
+            return;
+
+        txtStatus.text = "TOUCHED";
+
+        if (raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
+        {
+            txtStatus.text = "TOUCHED, INSTANTIATE";
+            var hitPose = hits[0].pose;
+            Instantiate(raycastManager.raycastPrefab, hitPose.position, hitPose.rotation);
+        }
+    }
+}
